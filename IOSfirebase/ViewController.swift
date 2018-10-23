1d5d9b6d8a689f328af847366893501b774a78d5
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2018-10-23 15:56:16 -0500

    lol

--- IOSfirebase/ViewController.swift
+++ IOSfirebase/ViewController.swift
@@ -10,13 +10,25 @@ import UIKit
 import FirebaseDatabase
 
 class ViewController: UIViewController {
-
+    var databaseRefer:DatabaseReference!
+    var databaseHandle:DatabaseHandle!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        /*
         // Do any additional setup after loading the view, typically from a nib.
         let ref = Database.database().reference()
-        ref.child("alguien/apellido").setValue("Cuenca")
-        ref.childByAutoId().setValue(["nombre":"Justin","apellido":"Guerra Guerra"])
+        ref.child("alguien/nosoyoy").setValue("Algo")
+        //ref.childByAutoId().setValue(["nombre":"Justin","apellido":"Guerra Guerra"])
+        */
+        databaseRefer = Database.database().reference()
+        databaseRefer.child("name").childByAutoId().setValue("Visa1");
+        
+        databaseHandle = databaseRefer.child("name").observe(.childAdded,with:{(data) in
+            let name:String=(data.value as? String)!
+            debugPrint(name)
+        })
     }
 
     override func didReceiveMemoryWarning() {
