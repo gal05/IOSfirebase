b7b9df735e2750810493be79016fd1faa4cd496e
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2018-10-24 13:11:26 -0500

    stop porr ahora XDD

--- IOSfirebase/ViewController.swift
+++ IOSfirebase/ViewController.swift
@@ -19,9 +19,11 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var txtpassword: UITextField!
     
+            var pass="jose"
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         dataToStore["codigo"] = "107907"
         dataToStore["password"] = "123"
 
@@ -29,19 +31,21 @@ class ViewController: UIViewController {
       /* let userDbRef = databaseRefer.child("usuarios").child("g")
         userDbRef.child("usuarioDetalles").setValue(dataToStore)
         */
-        
         databaseRefer.child("usuarios").child("g").child("usuarioDetalles").observeSingleEvent(of: .value, with: {(snapshot) in
             let value = snapshot.value as? NSDictionary
-            
             let codigo = value?["codigo"] as? String ?? ""
             let password = value?["password"] as? String ?? ""
-            
             print("datos = \(password)  \(codigo)")
             
         })
 
     }
-
+    @IBAction func buttonIngresar(_ sender: Any) {
+        
+        let data:String!=txtcodigo.text
+        print(data)
+        print(pass)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
